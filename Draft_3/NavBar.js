cdbfcb2e74888f1565f4f06fdb95ff6115ec128c
prathameshkurunkar7 <59260326+prathameshkurunkar7@users.noreply.github.com>
2020-01-19 20:26:59 +0530

    Add files via upload

---
+++ Draft_3/NavBar.js
@@ -0,0 +1,9 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
