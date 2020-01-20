eadf4e73498368e8528a6bfec3e20499e95a6220
Ketki Nirantar <ketki192000@gmail.com>
2020-01-20 06:37:35 +0530

    Add files via upload

---
+++ source/source/accounts/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout
+)
+
+from .forms import UserLoginForm, UserRegisterForm
+from django.urls import reverse
+
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+
+
+def login_view(request):
+    next = request.GET.get('next')
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+
+    context = {
+        'form': form,
+    }
+    return render(request, "login.html", context)
+
+
+def register_view(request):
+    next = request.GET.get('next')
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+
+    context = {
+        'form': form,
+    }
+    return render(request, "signup_k.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
